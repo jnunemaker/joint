d27ff92026371d2bf191fd948a5caf98d7a3f109
John Nunemaker <nunemaker@gmail.com>
2010-12-30 00:53:00 -0500

    Same order as elsewhere, name, size, type.

--- lib/joint/class_methods.rb
+++ lib/joint/class_methods.rb
@@ -37,8 +37,8 @@ module Joint
           else
             send("#{name}_id=", BSON::ObjectId.new) if send("#{name}_id").nil?
             send("#{name}_name=", Joint.name(file))
-            send("#{name}_type=", Joint.type(file))
             send("#{name}_size=", Joint.size(file))
+            send("#{name}_type=", Joint.type(file))
             assigned_attachments[:#{name}] = file
             nil_attachments.delete(:#{name})
           end
